6930aaff7c4feee9c3ce1e2502b5c62dfe04e5a3
henryemdth <henry.emdth@gmail.com>
2026-07-24 14:56:39 -0400

    chore(setting): update setting to deploy on Vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-      },
-      "/socket.io": {
-        target: "http://localhost:3001",
-        ws: true,
+
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), "");
+  const BACKEND_URL = env.VITE_BACKEND_URL || "http://localhost:3001";
+  return {
+    
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
       },
     },
-  },
+    server: {
+      port: 5173,
+      proxy: {
+        "/api": {
+          target: BACKEND_URL || "http://localhost:3001",
+          changeOrigin: true,
+        },
+        "/socket.io": {
+          target: BACKEND_URL ||"http://localhost:3001",
+          ws: true,
+        },
+      },
+    },
+  }
 });
